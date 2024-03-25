80149dc0b9efb18ac6e61fb46c1268dfc102e2ea
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-03-25 10:15:22 +0100

    Login successfull (in emulator): Fixing Error: CLEARTEXT communication to localhost not permitted bu network s

--- app/src/main/java/com/example/gieldadyzurowa/network/RetrofitService.kt
+++ app/src/main/java/com/example/gieldadyzurowa/network/RetrofitService.kt
@@ -5,7 +5,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitService {
     //private const val BASE_URL = "https://powerful-sea-67789-a7c9da8bf02d.herokuapp.com/"
-    private const val BASE_URL = "http://localhost:3000/"
+    //private const val BASE_URL = "http://localhost:3000/"
+
+    private const val BASE_URL = "http://10.0.2.2:3000/"
 
     fun <T> createService(serviceClass: Class<T>): T {
         return Retrofit.Builder()
